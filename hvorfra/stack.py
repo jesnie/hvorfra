73e42341b746c6a78b22e21a7e8723878ee3129f
Jesper Nielsen <jespernielsen1982@gmail.com>
2025-08-27 10:51:27 +0200

    Add function for getting caller location.

---
+++ hvorfra/stack.py
@@ -0,0 +1,24 @@
+from inspect import currentframe, getframeinfo
+from pathlib import Path
+
+from hvorfra.types import PARENT, CodeLocation
+
+
+def get_caller_location(depth: int = PARENT) -> CodeLocation | None:
+    frame = currentframe()
+    for _ in range(depth + 1):
+        if frame is None:
+            return None
+        frame = frame.f_back
+    assert frame is not None
+
+    frame_info = getframeinfo(frame)
+    positions = frame_info.positions
+    if positions is None or not isinstance(positions.col_offset, int):
+        return None
+
+    return CodeLocation(
+        Path(frame_info.filename),
+        frame_info.lineno,
+        positions.col_offset,
+    )
